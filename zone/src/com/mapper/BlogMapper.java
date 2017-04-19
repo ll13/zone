8749a090714bf61392e6dfd4e06e8eec3c93349b
mort <2573981093@qq.com>
2017-04-19 14:56:59 +0800

    修改了blog的实例导致删除的问题。罪过。

--- zone/src/com/mapper/BlogMapper.java
+++ zone/src/com/mapper/BlogMapper.java
@@ -7,6 +7,4 @@ import com.po.Blog;
 
 
 public interface BlogMapper {
-	 public int addContent(Blog blog);
-	 public List<Blog> getBlog(Blog blog);
 }
--- zone/src/com/mapper/Blog_CommentMapper.java
+++ zone/src/com/mapper/Blog_CommentMapper.java
@@ -5,6 +5,4 @@ import java.util.List;
 import com.po.Blog_Comment;
 
 public interface Blog_CommentMapper {
-	public int addBlog_Comment(Blog_Comment Blog_Comment);
-	public List<Blog_Comment> showBlog_Comment(Blog_Comment Blog_Comment);
 }
